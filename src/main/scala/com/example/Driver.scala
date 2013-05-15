c5ce841f07d740ac98eb097abc3debd1feaf6ee4
Ian O Connell <ian@knobout.com>
2013-05-15 13:39:53 -0700

    Blancas as specified on mailing list issue

--- src/main/scala/com/example/Driver.scala
+++ src/main/scala/com/example/Driver.scala
@@ -6,8 +6,51 @@ import spark.SparkContext._
 object Driver {
   def main(args: Array[String]) {
     val sc = Setup.GetSC("SampleProject")
-    val input_list = List(1, 2, 3)
-    println(input_list == sc.parallelize(input_list).collect.toList)
+
+    def manhattan(p: Map[Int, Double], q: Map[Int,Double]): Double = {
+      val diffs = for ((k, x) <- p; y = q.get(k); if y != None)
+                    yield { Math.abs(x - y.get) }
+      diffs.sum
+    }
+
+    val input = sc.textFile("intro.csv")
+
+    val data = input.map(line => {
+      val Array(user, item, rating) = line.split(",")
+      (user.toInt, item.toInt -> rating.toDouble)
+    }).groupByKey.map(grouped => {
+      val (user, ratings) = grouped
+      var ratMap = Map[Int, Double]()
+      ratings.foreach(r => ratMap += r)
+      (user, ratMap)
+    }).cache()
+
+    // Make n recommendations for user id using its nearest neighbor
+      val id = 1
+      val target = data.filter(user => {
+        val (uid, ratings) = user
+        uid == id
+      }).take(1) /////////// in the SBT app it hangs here, before take.  ///////////
+      val krat = target(0)._2
+      println("Nearest neighbor recs: " + krat)
+
+      // Remove the target user from the data file
+      val filtered = data.filter(user => {
+        val (uid, ratings) = user
+        uid != id
+      })
+
+      // Find the nearest neighborg
+      val neigh = filtered.map(user => {
+        val (uid, ratings) = user
+        (manhattan(ratings, krat), uid -> ratings)
+      }).sortByKey(false).take(1)
+      val neighRat = neigh(0)._2._2
+
+      val recs = for ((a, r) <- neighRat; if krat.get(a) == None)
+                   yield { (r, a) }
+      println(recs.toList.sorted.reverse.take(1))
+
     sc.stop
   }
 }
